eab82cf3e491e2b73c39ccd185c482173f40c594
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2020-04-05 18:28:57 +0200

    WIP

--- app/src/debug/java/pl/gov/mc/protego/ui/base/BaseVariantActivity.kt
+++ app/src/debug/java/pl/gov/mc/protego/ui/base/BaseVariantActivity.kt
@@ -1,22 +1,51 @@
 package pl.gov.mc.protego.ui.base
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.polidea.cockpit.cockpit.Cockpit
+import com.polidea.cockpit.event.ActionRequestCallback
 import org.koin.android.ext.android.inject
+import org.koin.core.context.GlobalContext
+import pl.gov.mc.protego.information.Session
+import pl.gov.mc.protego.information.SessionState
+import pl.gov.mc.protego.ui.registration.onboarding.OnboardingActivity
 
 abstract class BaseVariantActivity : AppCompatActivity() {
     private val shakeDetector: CockpitShakeDetector by inject()
+    private val logoutCallback: ActionRequestCallback = ActionRequestCallback {
+        val session: Session = GlobalContext().get().get()
+        if (session.sessionData.state == SessionState.LOGGED_IN) {
+            session.logout()
+            startActivity(Intent(this, OnboardingActivity::class.java))
+//            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initShakeDetection()
     }
 
+    override fun onStart() {
+        super.onStart()
+        initCockpit()
+    }
+
     override fun onStop() {
         super.onStop()
         shakeDetector.stopDetection()
+        deinitCockpit()
+    }
+
+    private fun initCockpit() {
+        Cockpit.addLogoutActionRequestCallback(this, logoutCallback)
+    }
+
+    private fun deinitCockpit() {
+        Cockpit.removeLogoutActionRequestCallback(logoutCallback)
     }
 
     private fun initShakeDetection() {
--- app/src/main/kotlin/pl/gov/mc/protego/ui/main/DashboardActivity.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/main/DashboardActivity.kt
@@ -1,14 +1,11 @@
 package pl.gov.mc.protego.ui.main
 
-import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_dashboard.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.gov.mc.protego.R
 import pl.gov.mc.protego.information.Session
 import pl.gov.mc.protego.ui.base.BaseActivity
-import pl.gov.mc.protego.ui.registration.onboarding.OnboardingActivity
 
 class DashboardActivity : BaseActivity() {
 
@@ -18,11 +15,6 @@ class DashboardActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        logout_button.setOnClickListener {
-            session.logout()
-            startActivity(Intent(this, OnboardingActivity::class.java))
-            finish()
-        }
     }
 
     override fun onResume() {
